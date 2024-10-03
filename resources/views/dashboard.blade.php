e993f968e9dde7e48f1a6386716676bbba1a583a
JakobVapper <jakob.vapper@ametikool.ee>
2024-10-03 23:44:30 +0300

    Testing the style changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,14 +4,17 @@
     <title>Dashboard</title>
     @vite('resources/css/app.css')
 </head> 
-<body class="flex items-center justify-center min-h-screen bg-gray-100"> 
+<body class="flex justify-center bg-gray-100 min-h-screen pt-10"> 
     <div class="text-center"> 
         <h1 class="text-5xl font-semibold mb-8">Dashboard</h1> 
         <div class="space-y-4"> 
-            <a class="block w-64 h-24 text-center leading-[6rem] bg-emerald-400 hover:bg-emerald-200 text-white rounded-lg" href="{{ route('products.index') }}"> Products </a> 
-            <a class="block w-64 h-24 text-center leading-[6rem] bg-teal-400 hover:bg-teal-200 text-white rounded-lg" href="{{ route('categories.index') }}"> Categories </a> 
+            <a class="w-10 text-center border-2 rounded-lg" href="{{ route('products.index') }}"> 
+                Products 
+            </a> 
+            <a class="w-10 text-center border-2 rounded-lg" href="{{ route('categories.index') }}"> 
+                Categories 
+            </a> 
         </div> 
     </div> 
 </body> 
-
 </html>
