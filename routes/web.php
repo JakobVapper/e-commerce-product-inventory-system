60acb8b487dc61493981c60a12d34685ef75439f
JakobVapper <jakob.vapper@ametikool.ee>
2024-10-03 22:24:12 +0300

    Added the routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
 
+Route::resource('categories', CategoryController::class);
 Route::resource('products', ProductController::class);
 
 Route::get('/', function () {
